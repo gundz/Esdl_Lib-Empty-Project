0d266bc5a6ab5b594353215de914407d7d376631
gundz <fgundlac@student.42.fr>
2015-08-09 20:22:00 +0200

    updated to lasted Esdl_Lib and added nicer demo

--- srcs/main.c
+++ srcs/main.c
@@ -1,24 +1,32 @@
 #include <easy_sdl.h>
 
+#define SPEED 3
+#define SIZE_X 50
+#define SIZE_Y 50
 
 void					test(t_esdl *esdl)
 {
-	static SDL_Rect		rect = {0, 0, 200, 200};
+	static SDL_Rect		rect = {0, 0, SIZE_X, SIZE_Y};
 	static SDL_Texture 	*square = NULL;
 	SDL_Surface			*surf;
+	static int			dirX = 1;
+	static int			dirY = 1;
 
 	if (square == NULL)
 	{
-		surf = Esdl_create_surface(200, 200);
+		surf = Esdl_create_surface(SIZE_X, SIZE_Y);
 		Esdl_draw_square(surf, rect, 0xFFFFFFFF);
 		square = SDL_CreateTextureFromSurface(esdl->en.ren, surf);
 	}
-	if (rect.x >= esdl->en.rx - 200)
-		rect.x = 0;
-	if (rect.y >= esdl->en.ry - 200)
-		rect.y = 0;
-	rect.x++;
-	rect.y++;
+
+	if (rect.x + SIZE_X >= esdl->en.rx || rect.x < 0)
+		dirX *= -1;
+	if (rect.y + SIZE_Y >= esdl->en.ry || rect.y < 0)
+		dirY *= -1;
+
+	rect.x += (dirX * SPEED);
+	rect.y += (dirY * SPEED);
+
 	SDL_RenderClear(esdl->en.ren);
 	SDL_RenderCopy(esdl->en.ren, square, NULL, &rect);
 	SDL_RenderPresent(esdl->en.ren);
@@ -28,7 +36,7 @@ int					main(int argc, char **argv)
 {
 	t_esdl			esdl;
 
-	if (Esdl_init(&esdl) == -1)
+	if (Esdl_init(&esdl, 640, 480, 60, "Engine") == -1)
 		return (-1);
 	while (esdl.run)
 	{
